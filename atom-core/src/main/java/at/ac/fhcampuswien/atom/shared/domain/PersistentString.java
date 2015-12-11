3d4be94921a3ac108ff40b755c59ff6f6ed36ef8
Thomas Käfer <thomas.kaefer@fh-campuswien.ac.at>
2015-12-11 15:30:30 +0100

    PersistentString - switch from type(int) to attribute name

--- atom-core/src/main/java/at/ac/fhcampuswien/atom/shared/domain/PersistentString.java
+++ atom-core/src/main/java/at/ac/fhcampuswien/atom/shared/domain/PersistentString.java
@@ -30,7 +30,7 @@ public class PersistentString extends DomainObject {
 
 	private String ownersAttribute;
 
-	private int type;
+//	private int type;
 
 	public PersistentString() {
 	}
@@ -63,13 +63,13 @@ public class PersistentString extends DomainObject {
 		this.ownersAttribute = ownersAttribute;
 	}
 
-	public void setType(int type) {
-		this.type = type;
-	}
-
-	public int getType() {
-		return type;
-	}
+//	public void setType(int type) {
+//		this.type = type;
+//	}
+//
+//	public int getType() {
+//		return type;
+//	}
 
 	@Override
 	public String getStringRepresentation() {
@@ -82,3 +82,43 @@ public class PersistentString extends DomainObject {
 		return value;
 	}
 }
+
+/*
+
+##### delete PersistentStrings with no owner
+DELETE
+FROM [I@H].[dbo].[PersistentString]
+WHERE owner_objectID IS NULL
+
+SELECT *
+FROM [I@H].[dbo].[PersistentString] p
+WHERE NOT EXISTS (SELECT d.objectID FROM [I@H].[dbo].[DomainObject] d where d.objectID = p.owner_objectID)
+--> gives empty result set
+
+SELECT *
+FROM [I@H].[dbo].[PersistentString] p
+WHERE NOT EXISTS (SELECT k.objectID FROM [I@H].[dbo].[Kontakt] k where k.objectID = p.owner_objectID)
+  AND NOT EXISTS (SELECT a.objectID FROM [I@H].[dbo].[AuslaendischeInstitution] a where a.objectID = p.owner_objectID)
+--> gives empty result set
+
+UPDATE p
+  SET p.[ownersAttribute] = 'arbeitsschwerpunkteMulti'
+  FROM [I@H].[dbo].[PersistentString] AS p
+  INNER JOIN [I@H].[dbo].[Kontakt] AS k
+  ON k.objectID = p.owner_objectID
+
+UPDATE p
+  SET p.[ownersAttribute] = 'departmentsFachbereiche'
+  FROM [I@H].[dbo].[PersistentString] AS p
+  INNER JOIN [I@H].[dbo].[AuslaendischeInstitution] AS k
+  ON k.objectID = p.owner_objectID
+  WHERE p.type = 1
+  
+UPDATE p
+  SET p.[ownersAttribute] = 'forschungsfelder'
+  FROM [I@H].[dbo].[PersistentString] AS p
+  INNER JOIN [I@H].[dbo].[AuslaendischeInstitution] AS k
+  ON k.objectID = p.owner_objectID
+  WHERE p.type = 2
+
+ */
